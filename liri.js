a355472d247c23312cb7aeed793555a4be66fcb2
unknown <ilyrodriguez@gmail.com>
2019-04-28 22:22:57 -0400

    missing one function and a default

--- liri.js
+++ liri.js
@@ -7,18 +7,16 @@ var spotify = new Spotify(keys.spotify);
 
 var axios = require("axios");
 var moment = require('moment');
-var inquirer = require('inquirer');
+// var inquirer = require('inquirer');
 var fs = require('fs');
 
-moment().format();
-
 var inputOne = process.argv[2];
-var inputTwo = process.argv[3];
+var inputTwo = process.argv.slice(3).join(" ");
 
 var queryUrl = "http://www.omdbapi.com/?t=" + inputTwo + "&y=&plot=short&apikey=trilogy";
 
 // This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+// console.log(queryUrl);
 
 // function theSwich ()
 switch (inputOne) {
@@ -40,8 +38,12 @@ switch (inputOne) {
     }
 
     function movieThis() { 
+    //   if (!inputTwo) {
+    //     inputTwo = "Mr. Nobody"
+    // };
     axios.get(queryUrl).then(
       function(response) {
+        if(inputTwo){
         console.log("Title: " + response.data.Title);
         console.log("Release Year: " + response.data.Year);
         console.log("Rating: " + response.data.imdbRating);
@@ -50,6 +52,17 @@ switch (inputOne) {
         console.log("Language: " + response.data.Language);
         console.log("Plot: " + response.data.Plot);
         console.log("Actors: " + response.data.Actors); 
+        } 
+        else {
+        console.log("Title: Mr. Nobody");
+        console.log("Release Year: 2009");
+        console.log("Rating: 7.8");
+        console.log("Rotten Tomatoes Rating: 67%");
+        console.log("Country of Origin: Belgium, Germany, Canada, France, USA, UK");
+        console.log("Language: English, Mohawk");
+        console.log("Plot: A boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilities arise from this decision. As long as he doesn't choose, anything is possible.");
+        console.log("Actors: Jared Leto, Sarah Polley, Diane Kruger, Linh Dan Pham"); 
+        }
       }
     ); }
 
@@ -58,12 +71,13 @@ function concertThis(){
   .then(function(response) {
     console.log("something anything!");
     console.log("Artist Name: " + inputTwo);
-    // console.log(response.venue);
-  //   console.log("still trying");
-  //   console.log(response.venue);
-  //   // console.log(response.venue.city + ", "+ venue.region + "; " + venue.country);
-  //   console.log("still trying");
-  // })
+    console.log("Venue: " + response.data[0].venue.name);
+    console.log("still trying");
+    console.log("Venue location: " + response.data[0].venue.city, response.data[0].venue.region);
+    // console.log(response.venue.city + ", "+ venue.region + "; " + venue.country);
+    console.log("date: " + response.data[0].moment(datetime).format("mm/dd/yyyy"));
+  })
+}
   // .catch(function(error) {
   //   if (error.response) {
   //     console.log(error.response.data);
@@ -75,11 +89,14 @@ function concertThis(){
   //     console.log("Error", error.message);
   //   }
   //   console.log(error.config);
-  });
-}
+//   });
+// }
 
 
 function spotifyThisSong () {
+  if (!inputTwo) {
+    inputTwo = "the sign ace of base"
+};
   spotify.search({ type: 'track', query: inputTwo}, function(err, data) {
     if (err) {
       return console.log('Error: ' + err);
@@ -88,17 +105,16 @@ function spotifyThisSong () {
   console.log("Song: " + data.tracks.items[0].name);
   console.log("Preview link: " + data.tracks.items[0].preview_url);
   console.log("Album: " + data.tracks.items[0].album.name);
-  })}
+  }
+  )}
   
-
-
   // function whatItSays (){
   //   fs.readFile('random.tx', 'utf8', function (err, data){
   //     if (err) throw err;
   //     var dataArr = data.split(',');
   //     if (dataArr.length == 2){
   //       pick(dataArr[0], dataArr[1]);
-  //     }else if (dataArr.lenght ==1){
+  //     }else if (dataArr.lenght == 1){
   //       pick(dataArr[0]);
   //     }
   //     });
